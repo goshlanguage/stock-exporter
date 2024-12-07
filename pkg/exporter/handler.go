a0de0f1abbc6fc13ab9c0eadaa4ce587dffd2ebc
Ryan Hartje <ryan@ryanhartje.com>
2024-12-07 12:13:11 -0600

    add liveness probe

--- pkg/exporter/handler.go
+++ pkg/exporter/handler.go
@@ -6,6 +6,11 @@ import (
 	"strings"
 )
 
+// Healthz returns status ok to satisfy health checks
+func (s *StockPriceExporter) Healthz(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 // SymbolHandler returns prices of symbols in our WatchList
 func (s *StockPriceExporter) SymbolHandler(w http.ResponseWriter, r *http.Request) {
 	var output []string
@@ -24,6 +29,7 @@ func (s *StockPriceExporter) Serve(address, port string) {
 	s.Logger.Sugar().Infof("StockPriceExporter listening on %s:%s", address, port)
 
 	http.HandleFunc("/metrics", s.SymbolHandler)
+	http.HandleFunc("/healthz", s.Healthz)
 
 	s.Logger.Sugar().Fatal(http.ListenAndServe(address+":"+port, nil))
 }
